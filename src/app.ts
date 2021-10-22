f77fe45874de5f10bd4b193bbda643e3d52d679d
cherryvue <yetiasg@gmail.com>
2021-10-22 12:26:42 +0200

    Change middleware and handlers implementations and initializations

--- src/app.ts
+++ src/app.ts
@@ -4,29 +4,31 @@ import cors from 'cors';
 import helmet from 'helmet';
 import mongoose from 'mongoose';
 import config from './config/config';
-import { Controller } from './api/v1/interfaces-types-abstracts/Controller.interface';
+import { Controller } from './api/v1/interfaces/Controller.interface';
 
 import { routeDoesNotExist } from './api/v1/middlewares/routeDoesNotExist.middleware';
 import { errorHandler } from './api/v1/middlewares/error.middleware';
 
 export class App{
   public app: express.Application;
-  constructor(controllers:Controller[], public port:number){
+
+  constructor(controllers:Controller[], public readonly port:number){
     this.app = express();
-    this._connectToDatabase();
+
+    this._connectToTheDatabase();
     this._initializeMiddlewares();
     this._initializeControllers(controllers);
-    this.app.use(errorHandler);
-    this.app.use(routeDoesNotExist)
+    this._initializeNotFoundHandler();
+    this._initializeErrorHandling();
   }
 
-  private _connectToDatabase(){
-    mongoose.connect(`${String(config.database.DB_HOST)}/back_ts`, () => {
+  private _connectToTheDatabase = () => {
+    mongoose.connect(`${String(config.database.DB_HOST)}/back_ts`, () =>{
       console.log("connected")
     })
   }
 
-  private _initializeMiddlewares(){
+  private _initializeMiddlewares = () =>{
     this.app.use(morgan('dev'));
     this.app.use(cors({
       allowedHeaders: ['Content-Type', 'Authorization']
@@ -36,14 +38,22 @@ export class App{
     this.app.use(express.json());
   }
 
-  private _initializeControllers(controllers:Controller[]){
+  private _initializeControllers = (controllers:Controller[]) =>{
     controllers.forEach((controller:Controller) => {
       this.app.use('/', controller.router)
     })
   }
 
+  private _initializeNotFoundHandler = () =>{
+    this.app.use(routeDoesNotExist)
+  }
+
+  private _initializeErrorHandling = () =>{
+    this.app.use(errorHandler);
+  }
+
   public listen(){
-    this.app.listen(this.port, () => {
+    this.app.listen(this.port, () =>{
       console.log(`Listening on port: ${this.port}`);
     });
   }
