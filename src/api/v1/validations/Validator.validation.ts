54c3c65b8f6076880ce931e067663516ef7217c3
cherryvue <yetiasg@gmail.com>
2021-10-23 20:57:04 +0200

    change registrationSchema

--- src/api/v1/validations/Validator.validation.ts
+++ src/api/v1/validations/Validator.validation.ts
@@ -10,7 +10,5 @@ export class Validator{
     email: Joi.string().email().lowercase().required().trim(),
     password: Joi.string().alphanum().min(8).max(30).required(),
     repeatPassword: Joi.ref('password'),
-    firstName: Joi.string().trim().required(),
-    lastName: Joi.string().required()
   })
 }
